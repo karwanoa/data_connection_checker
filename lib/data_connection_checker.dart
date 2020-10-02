13420acbf6cc697e0db07d16961755c4e13f35b2
karwanoa <31167079+karwanoa@users.noreply.github.com>
2020-10-02 22:16:30 +0300

    Update data_connection_checker.dart

--- lib/data_connection_checker.dart
+++ lib/data_connection_checker.dart
@@ -26,12 +26,12 @@ class DataConnectionChecker {
   ///
   /// Timeout is the number of seconds before a request is dropped
   /// and an address is considered unreachable
-  static const Duration DEFAULT_TIMEOUT = const Duration(seconds: 10);
+  static const Duration DEFAULT_TIMEOUT = const Duration(seconds: 5);
 
   /// Default interval is 10 seconds
   ///
   /// Interval is the time between automatic checks
-  static const Duration DEFAULT_INTERVAL = const Duration(seconds: 10);
+  static const Duration DEFAULT_INTERVAL = const Duration(seconds: 5);
 
   /// Predefined reliable addresses. This is opinionated
   /// but should be enough. See https://www.dnsperf.com/#!dns-resolvers
